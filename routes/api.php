b889062c7ade542da81a196212b8a6aa2974d351
Iqbal <ext_iqbal.mohammadzainuddin@malaysiaairlines.com>
2025-09-21 17:47:19 +0800

    feat: add boards api route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BoardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +11,12 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'profile']);
+
+    // Board routes
+    Route::get('/boards', [BoardController::class, 'findAllBoards']);
+    Route::get('/boards/{board}', [BoardController::class, 'findBoard']);
+    Route::post('/boards', [BoardController::class, 'createBoard']);
+    Route::put('/boards/{board}', [BoardController::class, 'updateBoard']);
+    Route::delete('/boards/{board}', [BoardController::class, 'deleteBoard']);
 });
 
