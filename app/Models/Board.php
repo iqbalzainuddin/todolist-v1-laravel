eabc5cc44a8d0f5174fb13cc29b8a1038e398f4d
Iqbal <ext_iqbal.mohammadzainuddin@malaysiaairlines.com>
2025-09-21 13:46:22 +0800

    feat: define Board model

---
+++ app/Models/Board.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
