097c751539079a3682dfee8ed5f7ef50df7b64fb
Iqbal <60773534+iqbalzainuddin@users.noreply.github.com>
2025-09-24 05:58:16 +0800

    [Feat] #00001: Board that will contain to do item list (#4)
    * feat: define Board model
    
    * feat: add relationship to boards
    
    * feat: migration for boards table
    
    * feat: add error response
    
    * feat: complete CRUD operation for boards
    
    * feat: add boards api route
    
    ---------
    
    Co-authored-by: Iqbal <ext_iqbal.mohammadzainuddin@malaysiaairlines.com>

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Board;
+use App\Traits\ApiResponses;
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    use ApiResponses;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function findAllBoards(Request $request)
+    {
+        return $this->success(Board::where('owner_id', request()->user()->id)->get());
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function findBoard(Request $request, Board $board)
+    {
+        if ($board->owner_id !== $request->user()->id) {
+            return $this->error("NOT FOUND", 404);
+        }
+
+        $responseData = [
+            "board" => $board->setHidden(['owner_id', 'owner']),
+            "owner" => $board->owner->name,
+        ];
+
+        return $this->success($responseData);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function createBoard(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'description' => 'string',
+        ]);
+
+        $board = Board::create([
+            'name' => $fields['name'],
+            'description' => $fields['description'] ?? null,
+            'owner_id' => $request->user()->id,
+        ]);
+
+        return $this->success("CREATED", 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function updateBoard(Request $request, Board $board)
+    {
+        $fields = $request->validate([
+            'name' => 'string',
+            'description' => 'string',
+        ]);
+
+        if ($board->owner_id !== $request->user()->id) {
+            return $this->error("NOT FOUND", 404);
+        }
+
+        $board->update($fields);
+        $responseData = [
+            "board" => $board->setHidden(['owner_id', 'owner']),
+            "owner" => $board->owner->name,
+        ];
+
+        return $this->success($responseData);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function deleteBoard(Board $board)
+    {
+        if ($board->owner_id !== request()->user()->id) {
+            return $this->error("NOT FOUND", 404);
+        }
+
+        $board->delete();
+        return $this->success("DELETED", 204);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function boards()
+    {
+        return $this->hasMany(Board::class, 'owner_id');
+    }
 }
